2d2f58bcf15493692ec980aa5891eb6c6f071c56
DulanS <dulan.shamindha@synapsys.lk>
2021-02-25 21:47:25 +0530

    Changes

--- API/backend/routes/productRoute.js
+++ API/backend/routes/productRoute.js
@@ -63,15 +63,20 @@ router.post("/", async (req, res) => {
 
 router.put("/", async (req, res) => {
   try {
+    var ProductId = req.body.ProductId;
     var CategoryId = req.body.CategoryId;
-    var CategoryName = req.body.CategoryName;
-    // console.log(OrderId);
+    var Name = req.body.Name;
+    var Description = req.body.Description;
+    var BuyingPrice = req.body.BuyingPrice;
+    var SellingPrice = req.body.SellingPrice;
+    var Quantity = req.body.Quantity;
+    var Commission = req.body.Commission;
     mysqlConnection.beginTransaction((err) => {
       if (err) {
         throw err;
       }
       mysqlConnection.query(
-        `CALL EditCategory(${CategoryId},'${CategoryName}');`,
+        `CALL CreateProduct(${ProductId},${CategoryId},'${Name}','${Description}',${BuyingPrice},${SellingPrice},${Quantity},${Commission});`,
         (error, results, fields) => {
           if (error) {
             return mysqlConnection.rollback(() => {
@@ -87,7 +92,40 @@ router.put("/", async (req, res) => {
           });
         }
         console.log("success!");
-        res.send({ message: "Category Edited." });
+        res.send({ message: "Product Edited." });
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message);
+  }
+});
+
+router.delete("/", async (req, res) => {
+  try {
+    var ProductId = req.body.ProductId;
+    mysqlConnection.beginTransaction((err) => {
+      if (err) {
+        throw err;
+      }
+      mysqlConnection.query(
+        `CALL DeleteProduct(${ProductId});`,
+        (error, results, fields) => {
+          if (error) {
+            return mysqlConnection.rollback(() => {
+              throw error;
+            });
+          }
+        }
+      );
+      mysqlConnection.commit((err) => {
+        if (err) {
+          return mysqlConnection.rollback(() => {
+            throw err;
+          });
+        }
+        console.log("success!");
+        res.send({ message: "Product Deleted." });
       });
     });
   } catch (err) {
